5bfab953dcd3cc18cd928d393f3bfd34483861ae
LeetCode <593947521@qq.com>
2018-08-08 13:42:05 +0800

    Code file

---
+++ Code/1069/1069.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+bool cmp(char a, char b) {return a > b;}
+int main() {
+    string s;
+    cin >> s;
+    s.insert(0, 4 - s.length(), '0');
+    do {
+        string a = s, b = s;
+        sort(a.begin(), a.end(), cmp);
+        sort(b.begin(), b.end());
+        int result = atoi(a.c_str()) - atoi(b.c_str());
+        s = to_string(result);
+        s.insert(0, 4 - s.length(), '0');
+        cout << a << " - " << b << " = " << s << endl;
+    } while (s != "6174" && s != "0000");
+    return 0;
+}
